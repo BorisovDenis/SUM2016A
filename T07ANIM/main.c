4b94e5224629ff1d17da8b4c3da7dcaac9e7b499
BorisovDenis <doka_den@mail.ru>
2016-06-08 17:37:12 +0300

    Add files via upload

---
+++ T07ANIM/main.c
@@ -0,0 +1,66 @@
+/* FILE NAME: T07ANIM.C
+ * PROGRAMMER: DB3
+ * DATE: 08.06.2016
+ * PURPOSE: WinAPI windowed application sample.
+ */
+
+#include <stdlib.h>
+#include <math.h>
+
+#include <windows.h>
+
+/* Window class name */
+#define WND_CLASS_NAME "My Window Class"
+
+/* Forward references */
+LRESULT CALLBACK MyWinFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam );
+
+/* The main program function */
+INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    CHAR *CmdLine, INT CmdShow )
+{
+  WNDCLASS wc;
+  HWND hWnd;
+  MSG msg;
+
+  /* Register window class */
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hCursor = LoadCursor(NULL, IDC_HAND);
+  wc.hIcon = LoadIcon(NULL, IDI_EXCLAMATION);
+  wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+  wc.hInstance = hInstance;
+  wc.lpszClassName = WND_CLASS_NAME;
+  wc.lpszMenuName = NULL;
+  wc.lpfnWndProc = MyWinFunc;
+
+  if (!RegisterClass(&wc))
+  {
+    MessageBox(NULL, "Error register window class", "ERROR", MB_OK | MB_ICONERROR);
+    return 0;
+  }
+
+  /* Create window */
+  hWnd = CreateWindow(WND_CLASS_NAME,
+    "30!",
+    WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT, CW_USEDEFAULT,
+    CW_USEDEFAULT, CW_USEDEFAULT,
+    NULL, NULL, hInstance, NULL);
+  if (hWnd == NULL)
+  {
+    MessageBox(NULL, "Create window erroe", "ERROR", MB_OK | MB_ICONERROR);
+    return 0;
+  }
+
+  /* Show window */
+  ShowWindow(hWnd, CmdShow);
+  UpdateWindow(hWnd);
+
+  /* Run message loop */
+  while (GetMessage(&msg, NULL, 0, 0))
+    DispatchMessage(&msg);
+
+  return 30;
+} /* End of 'WinMain' function */
